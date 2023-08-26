b8c49cfad7a1ce068000e9258768b3515a7957b8
kasunmadhuranga <ggkasun92@gmail.com>
2023-08-26 18:02:45 +0530

    Create mysql database conection

---
+++ studentmark/src/main/java/com/studentmark/studentmark/StudentmarkApplication.java
@@ -0,0 +1,13 @@
+package com.studentmark.studentmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentmarkApplication.class, args);
+	}
+
+}
---
+++ studentmark/src/test/java/com/studentmark/studentmark/StudentmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentmark.studentmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
